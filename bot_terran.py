47373bc50979a30abc5bf17f11799229f9130c67
ocarter84 <oliver.t.carter@gmail.com>
2022-04-17 14:22:44 -0700

    updates

--- bot_terran.py
+++ bot_terran.py
@@ -111,7 +111,100 @@ class Oli_bot(sc2.BotAI):
     #async def build_supply(self,cc):
     #    await self.build(SUPPLYDEPOT, near=cc.position.towards(self.game_info.map_center,8))
         
-        
+    # distribute workers function rewritten, the default distribute_workers() function did not saturate gas quickly enough
+    async def distribute_workers(self, performanceHeavy=True, onlySaturateGas=False):
+        # expansion_locations = self.expansion_locations
+        # owned_expansions = self.owned_expansions
+        mineralTags = [x.tag for x in self.state.units.mineral_field]
+        # gasTags = [x.tag for x in self.state.units.vespene_geyser]
+        geyserTags = [x.tag for x in self.geysers]
+
+        workerPool = self.units & []
+        workerPoolTags = set()
+
+        # find all geysers that have surplus or deficit
+        deficitGeysers = {}
+        surplusGeysers = {}
+        for g in self.geysers.filter(lambda x:x.vespene_contents > 0):
+            # only loop over geysers that have still gas in them
+            deficit = g.ideal_harvesters - g.assigned_harvesters
+            if deficit > 0:
+                deficitGeysers[g.tag] = {"unit": g, "deficit": deficit}
+            elif deficit < 0:
+                surplusWorkers = self.workers.closer_than(10, g).filter(lambda w:w not in workerPoolTags and len(w.orders) == 1 and w.orders[0].ability.id in [AbilityId.HARVEST_GATHER] and w.orders[0].target in geyserTags)
+                # workerPool.extend(surplusWorkers)
+                for i in range(-deficit):
+                    if surplusWorkers.amount > 0:
+                        w = surplusWorkers.pop()
+                        workerPool.append(w)
+                        workerPoolTags.add(w.tag)
+                surplusGeysers[g.tag] = {"unit": g, "deficit": deficit}
+
+        # find all townhalls that have surplus or deficit
+        deficitTownhalls = {}
+        surplusTownhalls = {}
+        if not onlySaturateGas:
+            for th in self.townhalls:
+                deficit = th.ideal_harvesters - th.assigned_harvesters
+                if deficit > 0:
+                    deficitTownhalls[th.tag] = {"unit": th, "deficit": deficit}
+                elif deficit < 0:
+                    surplusWorkers = self.workers.closer_than(10, th).filter(lambda w:w.tag not in workerPoolTags and len(w.orders) == 1 and w.orders[0].ability.id in [AbilityId.HARVEST_GATHER] and w.orders[0].target in mineralTags)
+                    # workerPool.extend(surplusWorkers)
+                    for i in range(-deficit):
+                        if surplusWorkers.amount > 0:
+                            w = surplusWorkers.pop()
+                            workerPool.append(w)
+                            workerPoolTags.add(w.tag)
+                    surplusTownhalls[th.tag] = {"unit": th, "deficit": deficit}
+
+            if all([len(deficitGeysers) == 0, len(surplusGeysers) == 0, len(surplusTownhalls) == 0 or deficitTownhalls == 0]):
+                # cancel early if there is nothing to balance
+                return
+
+        # check if deficit in gas less or equal than what we have in surplus, else grab some more workers from surplus bases
+        deficitGasCount = sum(gasInfo["deficit"] for gasTag, gasInfo in deficitGeysers.items() if gasInfo["deficit"] > 0)
+        surplusCount = sum(-gasInfo["deficit"] for gasTag, gasInfo in surplusGeysers.items() if gasInfo["deficit"] < 0)
+        surplusCount += sum(-thInfo["deficit"] for thTag, thInfo in surplusTownhalls.items() if thInfo["deficit"] < 0)
+
+        if deficitGasCount - surplusCount > 0:
+            # grab workers near the gas who are mining minerals
+            for gTag, gInfo in deficitGeysers.items():
+                if workerPool.amount >= deficitGasCount:
+                    break
+                workersNearGas = self.workers.closer_than(10, gInfo["unit"]).filter(lambda w:w.tag not in workerPoolTags and len(w.orders) == 1 and w.orders[0].ability.id in [AbilityId.HARVEST_GATHER] and w.orders[0].target in mineralTags)
+                while workersNearGas.amount > 0 and workerPool.amount < deficitGasCount:
+                    w = workersNearGas.pop()
+                    workerPool.append(w)
+                    workerPoolTags.add(w.tag)
+
+        # now we should have enough workers in the pool to saturate all gases, and if there are workers left over, make them mine at townhalls that have mineral workers deficit
+        for gTag, gInfo in deficitGeysers.items():
+            if performanceHeavy:
+                # sort furthest away to closest (as the pop() function will take the last element)
+                workerPool.sort(key=lambda x:x.distance_to(gInfo["unit"]), reverse=True)
+            for i in range(gInfo["deficit"]):
+                if workerPool.amount > 0:
+                    w = workerPool.pop()
+                    if len(w.orders) == 1 and w.orders[0].ability.id in [AbilityId.HARVEST_RETURN]:
+                        self.combinedActions.append(w.gather(gInfo["unit"], queue=True))
+                    else:
+                        self.combinedActions.append(w.gather(gInfo["unit"]))
+
+        if not onlySaturateGas:
+            # if we now have left over workers, make them mine at bases with deficit in mineral workers
+            for thTag, thInfo in deficitTownhalls.items():
+                if performanceHeavy:
+                    # sort furthest away to closest (as the pop() function will take the last element)
+                    workerPool.sort(key=lambda x:x.distance_to(thInfo["unit"]), reverse=True)
+                for i in range(thInfo["deficit"]):
+                    if workerPool.amount > 0:
+                        w = workerPool.pop()
+                        mf = self.state.mineral_field.closer_than(10, thInfo["unit"]).closest_to(w)
+                        if len(w.orders) == 1 and w.orders[0].ability.id in [AbilityId.HARVEST_RETURN]:
+                            self.combinedActions.append(w.gather(mf, queue=True))
+                        else:
+                            self.combinedActions.append(w.gather(mf))
 
 
         
